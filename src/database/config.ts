c9a3791c4f63fc10c9cfd1be97b9e1219f9046a8
navintc <navin.thamindu@gmail.com>
2024-03-16 18:51:34 +0530

    Adding comments for future referencing and adding .env

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,11 +1,15 @@
 import { Sequelize } from "sequelize-typescript";
 import { WeatherModel } from "../models/WeatherModel";
 
+// in this file the basic setting up for the database connection is made.
+// I added these values to a seperate .env file to improve security
+
 const connection = new Sequelize({
     dialect:"mysql",
-    host:"localhost",
-    username:"root",
-    password:"sql_ps",
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     logging:false,
     models:[WeatherModel],
 })
